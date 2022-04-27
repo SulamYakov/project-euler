1e7078fb0bceea088837c3276d91bff1638eb61d
Yaakov Vainshtein <weinsteinjw31@gmail.com>
2022-04-27 17:58:23 +0300

    added error case for multiples above the limit, and included a clause to handle the final answer in case the lcm is larger than the limit.

--- 1/p1.py
+++ 1/p1.py
@@ -13,6 +13,11 @@ mult1 = int(sys.argv[1])
 mult2 = int(sys.argv[2])
 lim   = int(sys.argv[3])
 
+# check that both multiples are less than the limit
+if mult1 > lim or mult2 > lim:
+  print("Argument error: the limit must be greater than both multiples.")
+  sys.exit()
+
 def adder_prog (m, l):
   ans = int((m/2) * math.floor((l-1)/m) * (math.floor((l-1)/m) + 1))
   return ans
@@ -22,6 +27,10 @@ def lcm (m1, m2):
   return ans
 
 # calculate final answer by first adding all multiples below the limit, then subtracting all common multiples from that result.
-finalAns = adder_prog(mult1, lim) + adder_prog(mult2, lim) - adder_prog(lcm(mult1, mult2), lim) 
+# this is not necessary if the lcm is above the limit. 
+if lcm(mult1, mult2) > lim:
+  finalAns = adder_prog(mult1, lim) + adder_prog(mult2, lim) 
+else:
+  finalAns = adder_prog(mult1, lim) + adder_prog(mult2, lim) - adder_prog(lcm(mult1, mult2), lim) 
 print(finalAns)
 
